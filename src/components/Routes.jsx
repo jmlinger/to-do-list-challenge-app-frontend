f765cf8e155aae585d8f78f8e06ec50b298c0066
jmlinger <jmlinger10@gmail.com>
2022-02-15 16:28:24 -0300

    feat: implementa a pagina de login.

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import Login from '../pages/Login';
+import Login from '../pages/Login';
 import ToDoList from '../pages/ToDoList';
 
 
 function allRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={ <Login /> } /> */}
+      <Route path="/" element={ <Login /> } />
       <Route path="/todolist" element={ <ToDoList /> } />
     </Routes>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// import './Login.css';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div id="login-body">
+      <fieldset id="login-fieldset">
+          <form id="login-form">
+            <input
+              data-testid="email-input"
+              type="email"
+              name="email"
+              // value={ email }
+              placeholder="Email"
+              // onChange={ handleChange }
+            />
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              // value={ password }
+              placeholder="Senha"
+              // onChange={ handleChange }
+            />
+            <Link to="/todolist">
+              <button id="login-button"
+                type="submit"
+                // disabled={ validateEmail() }
+                // onClick={ () => sendEmail(email) }
+              >
+                Entrar
+              </button>
+            </Link>
+          </form>
+        </fieldset>
+    </div>
+  );
+}
+
+export default Login;
