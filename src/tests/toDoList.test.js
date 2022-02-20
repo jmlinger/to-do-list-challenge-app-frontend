ca92d4519002058ce0fa65055d74e8eb2bf9d0c7
jmlinger <jmlinger10@gmail.com>
2022-02-20 16:09:12 -0300

    test: incrementa segundo teste.

--- src/tests/toDoList.test.js
+++ src/tests/toDoList.test.js
@@ -1,18 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import ToDoList from '../pages/ToDoList';
 import { renderWithRouterAndStore } from './testConfig';
 
-
 describe('Testes unitários da página de ToDoList.', () => {
   const taskNameId = 'taskName-input';
   const addTaskId = 'addTask-btn';
   const orderById = 'orderBy-select';
+  const defaultStatus = 'Pendente';
+  const taskList = [
+    'Tarefa 1'
+  ];
 
 
   test('Verifica se a página contêm os elementos esperados após ser renderizada.', () => {
-    renderWithRouterAndStore(<ToDoList />)
+    renderWithRouterAndStore(<ToDoList />);
 
     const taskNameInput = screen.getByTestId(taskNameId);
     const addTaskButton = screen.getByTestId(addTaskId);
@@ -21,5 +24,22 @@ describe('Testes unitários da página de ToDoList.', () => {
     expect(taskNameInput).toBeInTheDocument();
     expect(addTaskButton).toBeInTheDocument();
     expect(orderBySelect).toBeInTheDocument();
+  });
+
+  test('Verifica se tarefas são adicionadas.', () => {
+    renderWithRouterAndStore(<ToDoList />);
+
+    const taskNameInput = screen.getByTestId(taskNameId);
+    const addTaskButton = screen.getByTestId(addTaskId);
+
+    taskList.forEach((task) => {
+      userEvent.type(taskNameInput, task);
+      userEvent.click(addTaskButton);
+      const taskName = screen.getByText(task);
+      // const statusTH = screen.getByRole('columnheader', { name: 'Status' });
+      // const status = within(statusTH).getByRole('cell', { name: defaultStatus });
+      expect(taskName).toBeInTheDocument();
+      // expect(status).toBeInTheDocument();
+    });
   });
 });
